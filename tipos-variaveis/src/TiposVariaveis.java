37194e5326a41b0adb0c3460651ddbc1a6ae095e
Ricardo F. de Carvalho <ricardo.ferreiracarvalho@hotmail.com>
2023-02-17 14:38:44 -0300

    Tipos de variaveis aula 3

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
     }
 }
