6281a5bcd8d2728876e697e371b6d93959d50197
Ricardo F. de Carvalho <ricardo.ferreiracarvalho@hotmail.com>
2023-02-17 14:49:48 -0300

    AULA 04 Tipos variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,9 +1,29 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        /**
+         * Estudar:
+         * Tipos primitivos
+         * estudar a classe String que representa texto na aplicação;
+         */
+
+        String meuNome = "Ricardo Carvalho";
+
+
+
         double salarioMinimo = 2500.33;
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        System.out.println(numero);
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
     }
 }
